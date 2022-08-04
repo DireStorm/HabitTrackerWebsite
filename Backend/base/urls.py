a628ede48e427957b198886f9a7600bc90b00eed
Jerin Andrews <jerinandrews125@gmail.com>
2022-08-04 13:20:43 -0500

    Backend api setup for Registration 	new file:   Backend/base/__pycache__/api.cpython-39.pyc 	new file:   Backend/base/__pycache__/serializer.cpython-39.pyc 	modified:   Backend/base/__pycache__/urls.cpython-39.pyc 	modified:   Backend/base/__pycache__/views.cpython-39.pyc 	new file:   Backend/base/api.py 	new file:   Backend/base/serializer.py 	modified:   Backend/base/urls.py 	modified:   Backend/db.sqlite3

--- Backend/base/urls.py
+++ Backend/base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views #alternative 'from .views import *
-from .views import MyTokenObtainPairView 
+from .views import MyTokenObtainPairView
+from .api import RegisterApi
 #from .views import UInfoView
 
 from rest_framework_simplejwt.views import (
@@ -11,6 +12,7 @@ urlpatterns = [
     path('', views.getRoutes),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register', RegisterApi.as_view()),
     # path('', views.home, name="home"),
     # path('room/', views.room, name="room"),
     # path('users/', views.getUsers, name="users"),
